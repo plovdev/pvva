b59818ea209cf69cd4d24343d8301b8381c4964f
plovdev <app67253100@gmail.com>
2026-05-17 10:22:50 +0300

    update

--- pvva-cli/src/main/java/org/plovdev/pvvacli/PvvaPaths.java
+++ pvva-cli/src/main/java/org/plovdev/pvvacli/PvvaPaths.java
@@ -15,7 +15,7 @@ import static java.lang.System.getProperty;
 public final class PvvaPaths {
     public static final Path BUILD_XML = Path.of("build.xml");
     public static final Path PLUGIN_JSON = Path.of("plugin.json");
-    public static final Path PLUGINS_HOME = Path.of(getProperty("pv.home", getProperty("user.home")), "plugins");
+    public static final Path PLUGINS_HOME = Path.of(getProperty("pv.home", getProperty("user.home") + "/.PornViewer"), "plugins");
     public static final Path HTTP_CONFIG = Path.of("src/configs/http-config.json");
     public static final Path RESOURCE_CONFIG = Path.of("src/configs/resource-config.json");
     public static final Path MAIN_PARSER = Path.of("src/parsers/main-parser.parser");
